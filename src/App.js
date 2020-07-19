a95e3ccd2e96e4a3cc3c821bd62a5cd8dc630380
Gilbert Molina <gmolinac@outlook.com>
2020-07-18 22:55:37 -0600

    Fixing typo in Navbar

--- src/App.js
+++ src/App.js
@@ -22,15 +22,15 @@ export const App = () => {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link href="#home">Salary Detail</Nav.Link>
-          <Nav.Link href="#link">Montly Budget</Nav.Link>
+          <Nav.Link href="#link">Monthly Budget</Nav.Link>
           <Nav.Link href="#link">Remaining Money</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-          </NavDropdown>
+          </NavDropdown> */}
         </Nav>
         <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
